8e1de9fa085886bc4e6c421b454764436dac2e15
尹建鹏 <yinjianpeng@meituan.com>
2021-04-16 23:11:10 +0800

    second

---
+++ Frame01/Frame01/Frame01.h
@@ -0,0 +1,18 @@
+//
+//  Frame01.h
+//  Frame01
+//
+//  Created by yinjianpeng on 2021/4/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Frame01.
+FOUNDATION_EXPORT double Frame01VersionNumber;
+
+//! Project version string for Frame01.
+FOUNDATION_EXPORT const unsigned char Frame01VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Frame01/PublicHeader.h>
+
+
